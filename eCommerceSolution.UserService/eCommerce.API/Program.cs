6bf6dc87d19abf01ee0e4abd4ed354fe4d8b4a54
VASUDHA SHAH <vasudhashahbpr@gmail.com>
2025-03-22 14:28:24 +0530

    Add project files.

---
+++ eCommerceSolution.UserService/eCommerce.API/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Add Infrastructure Services
+builder.Services.AddInfrastructure();
+builder.Services.AddCore();
+
+//Add controllers to the services collection
+builder.Services.AddControllers();
+
+//Build the web application
+var app = builder.Build();
+
+//Routing
+app.UseRouting();
+
+//Auth
+app.UseAuthentication();
+app.UseAuthorization();
+
+//controller routes
+app.MapControllers();
+
+app.Run();
+
---
+++ eCommerceSolution.UserService/eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Core
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
