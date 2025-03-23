8506cbe2cd49aa5152bbd366117e71daef199d53
VASUDHA SHAH <vasudhashahbpr@gmail.com>
2025-03-23 16:29:01 +0530

    Users Repository

--- eCommerceSolution.UserService/eCommerce.Infrastructure/DependencyInjection.cs
+++ eCommerceSolution.UserService/eCommerce.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using eCommerce.Core.RepositoryContracts;
+using eCommerce.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace eCommerce.Infrastructure
 {
@@ -6,6 +8,7 @@ namespace eCommerce.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
+            services.AddSingleton<IUsersRepository, UsersRepository>();
             return services;
         }
     }
---
+++ eCommerceSolution.UserService/eCommerce.Infrastructure/Repositories/UsersRepository.cs
@@ -0,0 +1,28 @@
+﻿using eCommerce.Core.DTO;
+using eCommerce.Core.Entities;
+using eCommerce.Core.RepositoryContracts;
+
+namespace eCommerce.Infrastructure.Repositories
+{
+    internal class UsersRepository : IUsersRepository
+    {
+        public async Task<ApplicationUser?> AddUser(ApplicationUser user)
+        {
+            //Generate a new unique user ID for the user
+            user.UserId = Guid.NewGuid();
+            return user;
+        }
+
+        public async Task<ApplicationUser?> GetUserByEmailAndPassword(string? email, string? password)
+        {
+            return new ApplicationUser()
+            {
+                UserId = Guid.NewGuid(),
+                Email = email,
+                Password = password,
+                PersonName = "Person name",
+                Gender = GenderOptions.Male.ToString()
+            };
+        }
+    }
+}
