dd37713f6a5258b07531d92487fbd91428663df4
VASUDHA SHAH <vasudhashahbpr@gmail.com>
2025-03-23 18:57:20 +0530

    User Service

---
+++ eCommerceSolution.UserService/eCommerce.Core/ServiceContracts/IUsersService.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.Core.DTO;
+
+namespace eCommerce.Core.ServiceContracts
+{
+    /// <summary>
+    /// Contract for users service that contains use cases for users
+    /// </summary>
+    public interface IUsersService
+    {
+        /// <summary>
+        /// Method to handle user login use case and return an AuthenticationResponse object that contains status of login
+        /// </summary>
+        /// <param name="loginRequest"></param>
+        /// <returns></returns>
+       Task<AuthenticationResponse?> Login(LoginRequest loginRequest);
+
+        /// <summary>
+        ///  Method to handle user register use case and return an AuthenticationResponse object that contains status of registration
+        /// </summary>
+        /// <param name="registerRequest"></param>
+        /// <returns></returns>
+        Task<AuthenticationResponse?> Register(RegisterRequest registerRequest);
+
+    }
+}
---
+++ eCommerceSolution.UserService/eCommerce.Core/Services/UsersService.cs
@@ -0,0 +1,49 @@
+﻿using eCommerce.Core.DTO;
+using eCommerce.Core.Entities;
+using eCommerce.Core.RepositoryContracts;
+using eCommerce.Core.ServiceContracts;
+
+namespace eCommerce.Core.Services
+{
+    internal class UsersService : IUsersService
+    {
+        private readonly IUsersRepository _usersRepository;
+
+        public UsersService(IUsersRepository usersRepository)
+        {
+                _usersRepository = usersRepository;
+        }
+        public async Task<AuthenticationResponse?> Login(LoginRequest loginRequest)
+        {
+          ApplicationUser? user =  await _usersRepository.GetUserByEmailAndPassword(loginRequest.Email, loginRequest.Password);
+            if (user == null)
+            {
+                return null;
+            }
+            return new AuthenticationResponse(user.UserId, user.Email, user.PersonName, user.Gender, "token", Success: true);
+        }
+
+        public async Task<AuthenticationResponse?> Register(RegisterRequest registerRequest)
+        {
+            //create a new applicationuser object from registerrequest                                                                                                      
+            ApplicationUser? user = new ApplicationUser()
+            {
+                PersonName = registerRequest.PersonName,
+                Email = registerRequest.Email,
+                Password = registerRequest.Password,
+                Gender = registerRequest.Gender.ToString(),
+            };
+            ApplicationUser? registeredUser = await _usersRepository.AddUser(user);
+            if (user == null)
+            {
+                return null;
+            }
+            //REturn success response
+            return new AuthenticationResponse(
+                registeredUser.UserId,
+                registeredUser.Email,
+                registeredUser.PersonName, 
+                registeredUser.Gender, "token", Success: true);
+        }
+    }
+}
