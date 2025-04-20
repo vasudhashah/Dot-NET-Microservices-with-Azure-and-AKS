902dba621cf8af6cc027cd84ce41f5783d7007e3
VASUDHA SHAH <vasudhashahbpr@gmail.com>
2025-04-20 14:16:30 +0530

    Products Fluent Validation

---
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/Validators/ProductAddRequestValidators.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace BusinessLogicLayer.Validators
+{
+    public class ProductAddRequestValidators : AbstractValidator<ProductAddRequest>
+    {
+        public ProductAddRequestValidators()
+        {
+            //Product Name
+            RuleFor(temp => temp.ProductName).NotEmpty().WithMessage("Product Name cannot be empty");
+
+            //Category
+            RuleFor(temp => temp.Category).IsInEnum().WithMessage("Category cannot be blank");
+
+            //UnitPrice
+            RuleFor(temp => temp.UnitPrice).InclusiveBetween(0, double.MaxValue).WithMessage($"Unit price should be between 0 to {double.MaxValue}");
+
+            //QuantityInStock
+            RuleFor(temp => temp.QuantityInStock).InclusiveBetween(0, int.MaxValue).WithMessage($"Quantity in stock should be between 0 to {int.MaxValue}");
+        }
+    }
+}
