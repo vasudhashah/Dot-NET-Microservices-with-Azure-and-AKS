5016bd094ac38a00e205e90414f13ae69b0aa4c1
VASUDHA SHAH <vasudhashahbpr@gmail.com>
2025-04-28 14:59:37 +0530

    Editing Angular APP

---
+++ Angular App/src/environment.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:7207/api/auth/', // API base URL for development,
+    productsMicroserviceUrl: 'https://localhost:7049/api/products/'
+  };
+  
--- UsersService Angular App/src/environment.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:7207/api/auth/', // API base URL for development
-  };
-  
