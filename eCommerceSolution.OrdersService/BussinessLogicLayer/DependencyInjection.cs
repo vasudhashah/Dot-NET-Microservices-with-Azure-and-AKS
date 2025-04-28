f19bbb5488009eff41811ca4e06b9a1bec3b7c15
VASUDHA SHAH <vasudhashahbpr@gmail.com>
2025-04-28 17:23:08 +0530

    Orders Microservice base, Adding MongoDB

---
+++ eCommerceSolution.OrdersService/BussinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BussinessLogicLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            //TO DO: Add business logic layer services into the IoC container
+
+
+            return services;
+        }
+    }
+}
---
+++ eCommerceSolution.OrdersService/DataAccessLayer/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace DataAccessLayer
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add data access layer into the IOC Container
+            string connectionStringTemplate = configuration.GetConnectionString("MongoDB")!;
+            string connectionString =  connectionStringTemplate.Replace("$MONGO_HOST", Environment.GetEnvironmentVariable("MONGODB_HOST")).Replace("MONGO_PORT", Environment.GetEnvironmentVariable("MONGODB_PORT"));
+            services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+
+            services.AddScoped<IMongoDatabase>(provider =>
+            {
+                IMongoClient client =
+                provider.GetRequiredService<IMongoClient>();
+                return client.GetDatabase("OrdersDatabase");
+            });
+            return services;
+        }
+    }
+}
